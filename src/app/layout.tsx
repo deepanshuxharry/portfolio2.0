38fc063b4fe0fe654f816db34897510e99026bb7
deepanshu sharma <deepanshu43sharma@gmail.com>
2024-09-14 18:13:28 +0530

    Initial at commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,63 @@
+import type {Metadata, Viewport} from 'next';
+import {Analytics} from '@vercel/analytics/react';
+import {SpeedInsights} from '@vercel/speed-insights/next';
+import './globals.scss';
+import Navbar from '@/components/Navigations/NavBar';
+import Footer from '@/components/Navigations/Footer';
+
+export const metadata: Metadata = {
+    metadataBase: new URL('https://job-infos.kyawzinthiha.com'),
+    title: 'Deepanshu Sharma',
+    description: 'Versatile TypeScript FullStack Developer with a flair for designing and developing scalable web applications using Next.js, Nest.js, and a suite of modern technologies. Passionate about building user-centric solutions and leveraging AI technologies like OpenAI GPT for personalized experiences.',
+    keywords: 'TypeScript, Full Stack Developer, React.js, Node.js, Next.js, Nest.js, Web Development, AI, OpenAI GPT, Software Engineering',
+    authors: [{name: 'Deepanshu Sharma', url: 'https://kyawzinthiha.dev'}],
+    manifest: '/manifest.json',
+    appleWebApp: {
+        capable: true,
+        statusBarStyle: 'default',
+        title: 'Deepanshu Sharma'
+    },
+    formatDetection: {
+        telephone: false
+    },
+    openGraph: {
+        type: 'website',
+        locale: 'en_IE',
+        url: 'https://kyawzinthiha.dev',
+        images: [
+            {
+                url: 'https://job-infos.kyawzinthiha.com/profile-banner.png',
+                width: 1920,
+                height: 1080,
+                alt: 'Kyaw Zin Thiha - Full Stack Developer'
+            }
+        ]
+    }
+};
+
+export const viewport: Viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    viewportFit: 'cover',
+    themeColor: ' #191919',
+    colorScheme: 'dark'
+};
+
+
+export default function RootLayout({
+    children
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+        <body>
+        <Navbar/>
+        {children}
+        <Footer/>
+        <Analytics/>
+        <SpeedInsights/>
+        </body>
+        </html>
+    );
+}
---
+++ src/components/Sections/Home/index.tsx
@@ -0,0 +1,70 @@
+import Image from 'next/image';
+import Container from '@/components/Cards/Container';
+import GitHub from '@/resources/svgs/contact-icons/github.svg';
+import LinkedIn from '@/resources/svgs/contact-icons/linkedin.svg';
+import Mail from '@/resources/svgs/contact-icons/mail.svg';
+import Resume from '@/resources/svgs/resume.svg';
+import styles from './home.module.scss';
+import Code  from '@/resources/svgs/contact-icons/code.svg';
+
+export default function Home() {
+    return (
+        <Container id={'home'} className={'home'}>
+            <div className={styles['home-container']}>
+                <div className={styles.status}>
+                    <div className={styles['status-circle']}></div>
+                    Online.
+                </div>
+                <div className={styles.introduction}>
+                    Hey, I&apos;m Deepanshu Sharma
+                    <div className={styles.occupation}>
+                        Fullstack Developer.
+                    </div>
+                </div>
+                <div className={styles.contacts}>
+                    <a href="https://drive.google.com/file/d/1MqHrlQ012TFoRt4aPwoOT5tVzaXj-oFJ/view?usp=sharing" target="_blank"
+                       className={styles['resume-button']}>
+                        <Image src={Resume} alt={'GitHub'} className={styles.icon} width={20} height={20}/>
+                        <div className={styles.label}>Resume</div>
+                    </a>
+                    <a href="https://github.com/deepanshuxharry" target="_blank" className={styles.links}>
+                        <Image src={GitHub} alt={'GitHub'} className={styles.icon} width={20} height={20}/>
+                        <div className={styles.label}>GitHub</div>
+                    </a>
+                    <a href="https://www.linkedin.com/in/deepanshu-sharma-b7a122236/" target="_blank"
+                       className={styles.links}>
+                        <Image src={LinkedIn} alt={'LinkedIn'} className={styles.icon} width={18} height={18}/>
+                        <div className={styles.label}>LinkedIn</div>
+                    </a>
+                    <a href="mailto: deepanshu43sharma@gmail.com" className={styles.links}>
+                        <Image src={Mail} alt={'Mail'} className={styles.icon} width={22} height={22}/>
+                        <div className={styles.label}>Mail</div>
+                    </a>
+                    <a href="https://www.naukri.com/code360/profile/1e497968-e862-41f5-9003-d2de2205788e" className={styles.links}>
+                        <Image src={Code} alt={'Mail'} className={styles.icon} width={22} height={22}/>
+                        <div className={styles.label}>Code 360°</div>
+                    </a>
+                </div>
+                <hr className={styles.divider}/>
+                <div className={styles.summery}>
+                    <p>
+                        Hey!, I&apos;m Deepanshu Sharma, <b> Experienced full-stack developer </b>  with
+                        over 2
+                        years of
+                        experience
+                        in <b>React</b>,<b> Next</b>,<b> Node</b>.js with<b> MongoDB</b>. Ready to
+                        build innovative web applications and always putting out creative solutions to
+                        problems.
+                    </p><br/>
+                  
+                    {/* <p>
+                        When I’m not at the computer, I’m usually playing football, reading books, watching movies,
+                        relaxing at the
+                        beach thinking about the next big thing.
+                    </p> */}
+                </div>
+            </div>
+        </Container>
+    );
+}
+
